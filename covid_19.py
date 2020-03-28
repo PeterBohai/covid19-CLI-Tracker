caeb52895426db5efa01b5b7b105e338e1956e54
Peter Hu <peterbohai@gmail.com>
2020-03-28 14:24:10 -0700

    Add optional prompt to display other countries of interest

--- covid_19.py
+++ covid_19.py
@@ -7,7 +7,15 @@ import requests
 from tabulate import tabulate
 
 
-def extract_and_tabulate(target_countries_list):
+def in_terminal():
+    try:
+        os.get_terminal_size()
+    except OSError:
+        return False
+    return True
+
+
+def extract_data(target_countries_list):
     url = "https://www.worldometers.info/coronavirus/"
     source = requests.get(url).text
 
@@ -16,7 +24,7 @@ def extract_and_tabulate(target_countries_list):
     country_row = tbody.find_all("tr")
 
     # Extract wanted data
-    display_text = []
+    data_matrix = []
     for country in country_row:
         if country.find("td").get_text() in target_countries_list:
             data_cols = country.find_all("td")
@@ -28,52 +36,64 @@ def extract_and_tabulate(target_countries_list):
             new_cases = data_cols[2].get_text()
             new_deaths = data_cols[4].get_text()
 
-            display_text.append([name, cases, new_cases, deaths, new_deaths])
+            data_matrix.append([name, cases, new_cases, deaths, new_deaths])
 
-    display_text = sorted(display_text, key=lambda row: int(row[1].replace(",", "")), reverse=True)
-    table = tabulate(display_text,
-                     headers=["Country", "Cases", "New Cases", "Deaths", "New Deaths"],
-                     colalign=("left", "right", "left", "right", "left"),
-                     tablefmt="fancy_grid")
-    return table
+    data_matrix = sorted(data_matrix, key=lambda row: int(row[1].replace(",", "")), reverse=True)
+
+    return data_matrix
 
 
-def format_and_display(data_table, center_headers=True):
+def format_and_display_cli(data_table, center_headers=True):
     title_text = "Coronavirus (COVID-19) Tracker for Countries of Interest"
 
+    now_utc = datetime.datetime.now(pytz.timezone("UTC"))
     now_local = datetime.datetime.now()
     date_text = now_local.strftime("%b %d, %Y")
     time_text = now_local.strftime("%A %I:%M %p")
 
-    now_utc = datetime.datetime.now(pytz.timezone("UTC"))
     now_gmt = now_utc.astimezone(pytz.timezone("GMT"))
     gmt_text = "[" + now_gmt.strftime("%m-%d-%Y %I:%M %p %Z%z") + "]\n"
 
-    table_rows = data_table.split("\n")
+    # tabulate data to printable string
+    if not data_table:
+        term_width = 0
+        if in_terminal():
+            term_width = os.get_terminal_size().columns
+        print("Please enter at least one valid country name.".center(term_width))
+        return
+
+    display_table = tabulate(data_table,
+                             headers=["Country", "Cases", "New Cases", "Deaths", "New Deaths"],
+                             colalign=("left", "right", "left", "right", "left"),
+                             tablefmt="fancy_grid")
+    table_rows = display_table.split("\n")
     table_width = len(table_rows[0])
 
-    # format only headers to be centered, regardless of column alignment (not supported in tabulate)
-    if center_headers:
-        split_header = table_rows[1].split("│")
-        for i, header_str in enumerate(split_header):
-            width = len(header_str)
-            stripped_header = header_str.strip()
-            split_header[i] = stripped_header.center(width)
-        table_rows[1] = "│".join(split_header)
+    pad_left = 0
+    terminal_width = 0
 
-    try:
+    if in_terminal():
         terminal_width = os.get_terminal_size().columns
         pad_left = int(terminal_width / 2 - table_width / 2)
-    except OSError:
-        pad_left = 0
-        terminal_width = 0
 
     left_whitespace_offset = " " * pad_left
     note_text = ["  --> 'New' displays the live changes for the current day",
                  "      (reset after midnight GMT +0)",
                  "  --> Data acquired from www.worldometers.info/coronavirus/"]
 
+    # format only headers to be centered, regardless of column alignment (not supported in tabulate)
+    if center_headers:
+        split_header = table_rows[1].split("│")
+
+        for i, header_str in enumerate(split_header):
+            width = len(header_str)
+            stripped_header = header_str.strip()
+            split_header[i] = stripped_header.center(width)
+
+        table_rows[1] = "│".join(split_header)
+
     # Display data
+    print()
     print()
 
     print(title_text.center(terminal_width) + "\n")
@@ -92,9 +112,50 @@ def format_and_display(data_table, center_headers=True):
     print()
 
 
-countries_of_interest = ["USA", "China", "Italy", "Canada", "Thailand", "Taiwan"]
-covid19_table = extract_and_tabulate(countries_of_interest)
-format_and_display(covid19_table)
+def make_valid(str_entry):
+    varying_names = {"united states": "USA", "usa": "USA", "us": "USA", "america": "USA", "states": "USA",
+                     "united kingdom": "UK", "u.k.": "UK", "uk": "UK", "britain": "UK", "great britain": "UK",
+                     "south korea": "S. Korea",
+                     "hk": "Hong Kong",
+                     "uae": "UAE", "united arab emirates": "UAE", "emirates": "UAE",
+                     "drc": "DRC", "dr congo": "DRC", "democratic republic of the congo": "DRC",
+                     "car": "CAR", "central african republic": "CAR",
+                     "guinea bissau": "Guinea-Bissau",
+                     "timor leste": "Timor-Leste"}
+    clean_str = str_entry.strip().lower()
+
+    if clean_str in varying_names:
+        return varying_names[clean_str]
+
+    return clean_str.capitalize()
+
+
+if __name__ == "__main__":
+    default_countries = ["USA", "China", "Italy", "Canada", "Thailand", "Taiwan"]
+    countries_of_interest = []
+
+    width = 0
+    if in_terminal():
+        width = os.get_terminal_size().columns
+
+    print("Display default countries? (yes = ENTER)".center(width))
+    print(f"{default_countries}".center(width))
+    user_input_default = input(" " * (width // 2))
+
+    if not user_input_default.strip() or user_input_default.strip().lower() == "yes":
+        countries_of_interest = default_countries
+    else:
+        print()
+        print("Please specify countries of interest".center(width))
+        user_input = input("[separate by commas] [Enter blank for default]\n".center(width))
+        countries_of_interest = list(map(make_valid, user_input.split(",")))
+
+        while not countries_of_interest:
+            user_input = input("No countries specified, please try again: ")
+            countries_of_interest = list(map(make_valid, user_input.split(",")))
+
+    covid19_table = extract_data(countries_of_interest)
+    format_and_display_cli(covid19_table)
 
 
 
